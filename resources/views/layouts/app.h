83d104c3130558a1e13cd98109165b1f2ec00d58
Dark.Hades <dark.hades.1102@gmail.com>
2017-05-24 00:09:32 +0900

    Create view layouts

---
+++ resources/views/layouts/app.h
@@ -0,0 +1,15 @@
+#ifndef APP_CORE_VIEW_H_
+#define APP_CORE_VIEW_H_
+#include <cppcms/view.h>
+#include <string>
+
+namespace views {
+	namespace layouts
+	{
+		struct app : public cppcms::base_content {
+			std::string text;
+		};
+	}
+}
+
+#endif
